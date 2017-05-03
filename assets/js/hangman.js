72bc7e0e4d309ce67c2776231617340947886d39
Caleb Bux <Caleb Bux>
2017-05-03 15:35:24 -0400

    updated word list

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -1,4 +1,4 @@
-var words = ["active", "beyond", "center"]
+var words = ["active", "beyond", "center", "cartman", "stan", "kyle", "hankey", "kenny", "chef", "garrison", "slave", "jesus"]
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var guessedLettersArr = [];
 var correctLettersArr =[];
